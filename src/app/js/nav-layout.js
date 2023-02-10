b988cbb5d6ec846c976f0d58599a65990959c55f
Artur SD <arko8919@gmail.com>
2023-02-10 18:03:19 +0000

    content fixes

--- src/app/js/nav-layout.js
+++ src/app/js/nav-layout.js
@@ -1,19 +1,21 @@
-export function changeMenu(buttonRef, navRef) {
-    // disable and enable menu items on small devices
-    buttonRef.addEventListener('click', function () {
-        if (navRef.getAttribute('class') === 'menu') {
-            navRef.setAttribute('class', 'menu active');
-        } else {
-            navRef.setAttribute('class', 'menu');
-        }
-    });
+export function changeMenu(navIconRef, navRef) {
+  // Disable and enable menu items on small devices
+  navIconRef.addEventListener("click", function () {
+    if (navRef.getAttribute("class") === "menu") {
+      navRef.setAttribute("class", "menu active");
+      navIconRef.setAttribute("class", "nav-icon open");
+    } else {
+      navRef.setAttribute("class", "menu");
+      navIconRef.setAttribute("class", "nav-icon");
+    }
+  });
 
-    // when viewport change, it makes sure that menu items shows on large devices and hide on small devices
-    window.addEventListener('resize', function () {
-        if (window.innerWidth > '1024') {
-            navRef.setAttribute('class', 'menu active');
-        } else {
-            navRef.setAttribute('class', 'menu');
-        }
-    });
+  // When viewport change, it makes sure that menu items shows on large devices and hide on small devices
+  window.addEventListener("resize", function () {
+    if (window.innerWidth > "1024") {
+      navRef.setAttribute("class", "menu active");
+    } else {
+      navRef.setAttribute("class", "menu");
+    }
+  });
 }
--- src/index.js
+++ src/index.js
@@ -4,20 +4,17 @@ import { changeMenu } from './app/js/nav-layout';
 import { projectsGallery } from './app/js/gallery';
 
 // references - mobile menu
-const buttonRef = document.querySelector('.icon-nav');
-const navRef = document.querySelector('.menu');
+const navIconRef = document.querySelector('.nav-icon'); // menu bar
+const navRef = document.querySelector('.menu'); // list of menu items
 
 
 // references - gallery images
-const figCapRef = document.querySelectorAll('.my-work figcaption');
 const figureRef = document.querySelectorAll('.my-work figure');
 const topCaptionRef = document.querySelectorAll('.my-work figcaption div:nth-child(1)');
 const bottomCaptionRef = document.querySelectorAll('.my-work figcaption div:nth-child(2)');
-const captionRef = document.querySelectorAll('.caption');
 
 const button = document.querySelectorAll('.button');
 
-
-changeMenu(buttonRef, navRef);
+changeMenu(navIconRef, navRef);
 projectsGallery(figureRef, topCaptionRef, bottomCaptionRef, button);
 
